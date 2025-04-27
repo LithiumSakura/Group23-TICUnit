6da29d2578df4e9584a24d478c917b1738fd51c2
Setudo <samuelellisrobertson@gmail.com>
2025-04-27 17:15:05 +0100

    Added a function for checking which gates are busy - waiting for database to implement notifications

--- notification-system.py
+++ notification-system.py
@@ -1,5 +1,6 @@
 import smtplib
 from email.mime.text import MIMEText
+from crowd_counter import crowd_detection
 
 # Email Information
 sender = "airportmanagementgroup23@gmail.com"
@@ -8,7 +9,16 @@ password = "bkpq alsr olqq mbfd" # Google app password
 title = "Airport Management System Notification"
 text = "This is a test notification for the email sender system that will be changed in the future"
 
+def get_busy_gates(): # Returns a list of busy gates
+    gate_traffic = crowd_detection.count_people_at_gates()
+    busy_gates = []
+    for i in range(len(gate_traffic)):
+        if gate_traffic[i] >= 20:
+            busy_gates.append(i+1)
+    return busy_gates
+
 # MAKE AN AREA TO GRAB RECIPIENT INFO FROM DATABASE
+# Once database is implemented this function will get the email of all users that have flights near a busy gate OR a flight that is soon to depart
 
 
 
@@ -26,9 +36,9 @@ text = "This is a test notification for the email sender system that will be cha
 reciever = [sender, "samuelellisrobertson@gmail.com", "ben@woodwardfamily.eu", "oisinwillisdabomb@gmail.com", "pelayoanglada05@gmail.com"] # Change to program that gets based off gates
 
 
-def make_notification(type,area): # Type of notification and area in which it involves (area can be none)
+def make_notification(type,gate): # Type of notification and gate in which it involves (area can be none)
     if type.lower() == "crowd":
-        return f"Warning - {area} may be busy due to traffic."
+        return f"Warning - {gate} may be busy due to traffic."
     elif type.lower() == "reminder":
         return f"Reminder - your gate closes soon, please make your way to gate {area}."
     
@@ -45,4 +55,5 @@ def send_email(title, text, sender, reciever, password):
     print("Checkpoint - Sent!")
 
 print(reciever)
-send_email(title, text, sender, reciever, password)
+#send_email(title, text, sender, reciever, password)
+print(get_busy_gates())
